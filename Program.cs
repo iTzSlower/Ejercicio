701d18e6e780f8724612af472ea5541d04e3aa87
itzslowerdtc <itzslowerdtc@hotmail.com>
2023-03-28 20:35:47 -0500

    a

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la tienda");
+
+
+            string a = Console.ReadLine();
+        }
+
+
+    }
+    class Weapon
+    {
+        void WeaponDamage(int a)
+        {
+            return a = 0;
+        }
+    }
+}
